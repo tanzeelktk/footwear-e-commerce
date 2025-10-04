ccf52d1435423f257973991420eed76dcd1c2de0
Tanzeel ur Rehman <tanzeelktk07@gmail.com>
2025-10-05 00:20:49 +0500

    commit for routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,30 @@
 import './App.css'
 import {Button} from "flowbite-react"
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  Router,
+  RouterProvider,
+} from "react-router-dom";
+import AdminLayout from './layoutes/AdminLayout';
+import Dashboard from './pages/admin/Dashboard';
+import Products from './pages/admin/Products';
+import Orders from './pages/admin/Orders';
 
 function App() {
-
-  return (
-    <>
-      <div>
-        <h1 className='text-red-500'>Hello</h1>
-        <Button>Click me</Button>
-        
-      </div>
-      
-    </>
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+      <Route path='/admin' element={<AdminLayout/>}>
+        <Route index element={<Dashboard/>}/>
+        <Route path='product' element={<Products/>}/>
+        <Route path='order' element={<Orders/>}/>
+      </Route>
+      </>
+    )
   )
+  return <RouterProvider router={router}/>
 }
 
 export default App
