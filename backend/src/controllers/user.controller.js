a068ea62ba1dfb2732821018317e9d44d02ca986
Tanzeel ur Rehman <tanzeelktk07@gmail.com>
2025-10-16 10:52:05 +0500

    created order controllers

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcrypt");
 const { generateToken } = require("../utils/jwt");
 
 const userRegister = async (req, res) => {
-  const image = req.file.filename
+  const image = req.file.filename;
   const { name, email, password } = req.body;
   try {
     const userExist = await userModel.findOne({ email });
@@ -14,7 +14,7 @@ const userRegister = async (req, res) => {
       name: name,
       email: email,
       password: hashedPassword,
-      image
+      image,
     });
     const response = await user.save();
     res
@@ -22,6 +22,19 @@ const userRegister = async (req, res) => {
       .json({ message: " user created successfully", data: response });
   } catch (error) {
     res.status(400).json(error);
+  }
+};
+
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await userModel.find();
+    res.status(200).json({ message: "All users data.", data: users });
+  } catch (error) {
+    console.error("Error fetching users:", error.message);
+    res.status(500).json({
+      message: "Failed to fetch users.",
+      error: error.message,
+    });
   }
 };
 
@@ -35,24 +48,84 @@ const userLogin = async (req, res) => {
     if (!passCompare)
       return res.status(400).json({ message: "invalid credentials" });
     const payload = {
-      id:userExist._id,
-      email:userExist.email,
-      role:userExist.role
-    }
-    const token = await generateToken(payload)
+      id: userExist._id,
+      email: userExist.email,
+      role: userExist.role,
+    };
+    const token = await generateToken(payload);
     const resData = {
       name: userExist.name,
       email: userExist.email,
       isVerified: userExist.isVerified,
-      token:token
+      token: token,
     };
     res.status(200).json({ message: "user login successfully", data: resData });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 };
+
+const deleteUser = async (req, res) => {
+  try {
+    const userId = req.params.id;
+
+    // Check if the user exists
+    const userExist = await userModel.findById(userId);
+    if (!userExist) {
+      return res.status(404).json({ message: "User not found." });
+    }
+
+    // Delete user
+    const deleteResponse = await userModel.findByIdAndDelete(userId);
+
+    res.status(200).json({
+      message: "User deleted successfully.",
+      data: deleteResponse,
+    });
+  } catch (error) {
+    console.error("Error deleting user:", error.message);
+    res.status(500).json({
+      message: "Failed to delete user.",
+      error: error.message,
+    });
+  }
+};
+
+const updateUser = async (req, res) => {
+  try {
+    const userId = req.params.id
+    const {password, role} = req.body
+    const userExist = await userModel.findById(userId)
+    if(!userExist)
+      return res.status(404).json({message:"User not found."})
+    
+    const updateData = {}
+
+    if(password){
+      const hashedPassword = await bcrypt.hash(password, 10)
+      updateData.password = hashedPassword
+    }
+
+    if(role){
+      updateData.role = role
+    }
+
+    const updatedUser = await userModel.findByIdAndUpdate(userId, {$set:updateData}, {new:true})
+    res.status(200).json({message:"Updated successfully.", data:updatedUser})
+
+  } catch (error) {
+    console.error("Error updating user data:", error.message);
+    res.status(500).json({
+      message: "Failed to update user data.",
+      error: error.message,
+    });
+  }
+}
 
 module.exports = {
   userRegister,
   userLogin,
+  getAllUsers,
+  deleteUser,
+  updateUser
 };
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,10 +1,16 @@
 const express = require("express")
-const { userRegister, userLogin } = require("../controllers/user.controller")
+const { userRegister, userLogin, getAllUsers, deleteUser, updateUser } = require("../controllers/user.controller")
 const upload = require("../middlewares/upload.middleware")
+const { verifyToken } = require("../utils/jwt")
+const { checkRole } = require("../middlewares/auth.midleware")
 const userRoutes = express.Router()
 
 userRoutes.post("/register",upload.single("image"), userRegister)
 userRoutes.post("/login", userLogin)
+userRoutes.get("/users-list", verifyToken, checkRole(["admin"]), getAllUsers)
+userRoutes.delete("/delete-user/:id", verifyToken, checkRole(["admin"]), deleteUser)
+userRoutes.put("/update-user/:id", verifyToken, checkRole(["admin"]), updateUser)
+
 
 
 module.exports = userRoutes
